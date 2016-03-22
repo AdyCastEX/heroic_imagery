5d3e63f36fce73c3853c645c4a3a249ef403ccfb
Ady Castueras <ady.castueras@gmail.com>
2016-03-22 11:10:23 +0000

    create and delete hero image schema

--- includes/class-heroic-imagery-deactivator.php
+++ includes/class-heroic-imagery-deactivator.php
@@ -30,7 +30,18 @@ class Heroic_Imagery_Deactivator {
 	 * @since    1.0.0
 	 */
 	public static function deactivate() {
-
+		Heroic_Imagery_Deactivator::heroic_imagery_db_drop();
 	}
 
+	public static function heroic_imagery_db_drop(){
+		
+		global $wpdb;
+		//table name will be 'wp_hero_images'
+		$table_name = $wpdb->prefix . 'hero_images';
+		
+		//declare an SQL query to delete hero_images table
+		$sql = "DROP TABLE $table_name";
+		//run the SQL query to delete the table
+		$wpdb->query($sql);
+	}
 }
